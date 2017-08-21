9fe475eeb935da7eed84f1c35eae36f9bb847cf8
Christian Talmo <talmo.christian@gmail.com>
2017-08-21 03:15:24 +0300

    refactored actions, added quicker tests during MITERU_SUPER_TEST_OVERRIDE env variable

--- test/test.js
+++ test/test.js
@@ -11,6 +11,14 @@ var miteru = require('../src/index.js')
 
 var test = require('tape')
 
+var ACTION_INTERVAL = 500
+
+if ( !!process.env.MITERU_SUPER_TESTING_SPEED_OVERRIDE ) {
+  ACTION_INTERVAL = 50
+}
+
+// process.env.
+
 function run ( filepath ) {
   var resolved = require.resolve( filepath )
   delete require.cache[ resolved ]
@@ -56,7 +64,7 @@ function verifyFileCleaning ( files ) {
 }
 
 test( 'watch a single file', function ( t ) {
-  t.timeoutAfter( 2500 )
+  t.timeoutAfter( 5000 )
 
   prepareTestFiles(function () {
     var filepath = path.join( __dirname, 'tmp', 'main.js' )
@@ -128,13 +136,130 @@ test( 'watch a single file', function ( t ) {
       },
     ]
 
-    setTimeout( next, 300 )
+    setTimeout( next, ACTION_INTERVAL )
 
     function next () {
       var a = actions.shift()
       if ( a ) {
         a()
-        setTimeout( next, 300 )
+        setTimeout( next, ACTION_INTERVAL )
+      } else {
+        finish()
+      }
+    }
+
+    function finish () {
+      t.deepEqual(
+        buffer,
+        expected,
+        'expected output OK'
+      )
+
+      t.deepEqual(
+        w.getWatched(),
+        [ filepath ],
+        'expected files (1) still being watched'
+      )
+
+      w.unwatch( filepath )
+
+      t.deepEqual(
+        w.getWatched(),
+        [],
+        'expected files (0) still being watched'
+      )
+
+      w.close()
+
+      setTimeout( function () {
+        t.end()
+      }, 100 )
+    }
+  })
+})
+
+test( 'watch a single file -- file content appended between FSStat:ing', function ( t ) {
+  t.timeoutAfter( 5000 )
+
+  prepareTestFiles(function () {
+    var filepath = path.join( __dirname, 'tmp', 'main.js' )
+
+    var expected = [
+      ''
+      , 'init: abra'
+      , 'change: 88'
+      , 'unlink: ' + filepath
+      , 'add: 11'
+      , 'change: kadabra'
+      , 'change: allakhazam'
+    ]
+
+    var buffer = ['']
+
+    t.ok(
+      verifyFileCleaning(
+        [
+          filepath
+        ]
+      ),
+      'test pre-cleaned properly'
+    )
+
+    fs.writeFileSync( filepath, 'module.exports = "abra"' )
+
+    var w = miteru.watch( filepath, function ( evt, filepath ) {
+      switch ( evt ) {
+        case 'init':
+          buffer.push( 'init: ' + run( filepath ))
+          break
+
+        case 'add':
+          buffer.push( 'add: ' + run( filepath ) )
+          break
+
+        case 'unlink':
+          try {
+            fs.readFileSync( filepath )
+            t.fail( 'unlink event FAILURE (File still exists)' )
+          } catch ( err ) {
+            t.equal( err.code, 'ENOENT', 'unlink event OK (ENOENT)' )
+            buffer.push( 'unlink: ' + filepath )
+          }
+          break
+
+        case 'change':
+          buffer.push( 'change: ' + run( filepath ) )
+          break
+      }
+    })
+
+    var actions = [
+      function () {
+        fs.writeFileSync( filepath, 'module.exports = 88' )
+      },
+      function () {
+        rimraf.sync( filepath )
+      },
+      function () {
+        fs.writeFileSync( filepath, 'module.exports = 11' )
+      },
+      function () {
+        w._setDebugFlag( filepath, 'changeContentAfterFSStat', true )
+
+        fs.writeFileSync( filepath, 'module.exports = "kadabra"' )
+      },
+      function () {
+        fs.writeFileSync( filepath, 'module.exports = "allakhazam"' )
+      },
+    ]
+
+    setTimeout( next, ACTION_INTERVAL )
+
+    function next () {
+      var a = actions.shift()
+      if ( a ) {
+        a()
+        setTimeout( next, ACTION_INTERVAL )
       } else {
         finish()
       }
@@ -171,7 +296,7 @@ test( 'watch a single file', function ( t ) {
 })
 
 test( 'watch a non-existing file', function ( t ) {
-  t.timeoutAfter( 2500 )
+  t.timeoutAfter( 5000 )
 
   prepareTestFiles(function () {
     var filepath = path.join( __dirname, 'tmp', 'blabla.js' )
@@ -240,13 +365,13 @@ test( 'watch a non-existing file', function ( t ) {
       },
     ]
 
-    setTimeout( next, 200 )
+    setTimeout( next, ACTION_INTERVAL )
 
     function next () {
       var a = actions.shift()
       if ( a ) {
         a()
-        setTimeout( next, 200 )
+        setTimeout( next, ACTION_INTERVAL )
       } else {
         finish()
       }
@@ -283,7 +408,7 @@ test( 'watch a non-existing file', function ( t ) {
 })
 
 test( 'watch a new file after init', function ( t ) {
-  t.timeoutAfter( 2500 )
+  t.timeoutAfter( 5000 )
 
   prepareTestFiles(function () {
     var filepath = path.join( __dirname, 'tmp', 'filepath.js' )
@@ -366,13 +491,13 @@ test( 'watch a new file after init', function ( t ) {
       },
     ]
 
-    setTimeout(next, 200)
+    setTimeout( next, ACTION_INTERVAL )
 
     function next () {
       var a = actions.shift()
       if ( a ) {
         a()
-        setTimeout( next, 200 )
+        setTimeout( next, ACTION_INTERVAL )
       } else {
         finish()
       }
@@ -419,7 +544,243 @@ test( 'watch a new file after init', function ( t ) {
   })
 })
 
+test( 'watch a new file after init removed between FSStat:ing', function ( t ) {
+  t.timeoutAfter( 5000 )
+
+  prepareTestFiles(function () {
+    var filepath = path.join( __dirname, 'tmp', 'filepath.js' )
+    var filepath2 = path.join( __dirname, 'tmp', 'filepath2.js' )
+
+    var timestamp = Date.now()
+
+    var expected = [
+      ''
+      , 'init: abra'
+      , 'change: 88'
+      , 'unlink: ' + filepath
+      , 'add: 11'
+      , 'add: ' + timestamp // should be of evt type add and not init
+      , 'add: 22'
+    ]
+
+    var buffer = ['']
+
+    t.ok(
+      verifyFileCleaning(
+        [
+          filepath,
+          filepath2
+        ]
+      ),
+      'test pre-cleaned properly'
+    )
+
+    fs.writeFileSync( filepath, 'module.exports = "abra"' )
+
+    var w = miteru.watch( filepath, function ( evt, filepath ) {
+      switch ( evt ) {
+        case 'init':
+          buffer.push( 'init: ' + run( filepath ))
+          break
+
+        case 'add':
+          buffer.push( 'add: ' + run( filepath ) )
+          break
+
+        case 'unlink':
+          try {
+            fs.readFileSync( filepath )
+            t.fail( 'unlink event FAILURE (File still exists)' )
+          } catch ( err ) {
+            t.equal( err.code, 'ENOENT', 'unlink event OK (ENOENT)' )
+            buffer.push( 'unlink: ' + filepath )
+          }
+          break
+
+        case 'change':
+          buffer.push( 'change: ' + run( filepath ) )
+          break
+      }
+    })
+
+    var actions = [
+      function () {
+        fs.writeFileSync( filepath, 'module.exports = 88' )
+      },
+      function () {
+        rimraf.sync( filepath )
+      },
+      function () {
+        fs.writeFileSync( filepath, 'module.exports = 11' )
+      },
+      function () {
+        w.unwatch( filepath )
+        rimraf.sync( filepath )
+      },
+      function () {
+        w.add( filepath2 )
+        w._setDebugFlag( filepath2, 'removeAfterFSStat', true )
+
+        t.equal(
+          w.getLog( filepath2 )[ 'FSStatReadFileSyncErrors' ],
+          undefined,
+          'FSStatReadFileSyncErrors OK ( undefined )'
+        )
+
+        var content = ( 'module.exports = ' + timestamp )
+        fs.writeFileSync( filepath2, content )
+
+        try {
+          fs.readFileSync( filepath2 )
+          t.pass( 'file was created and should be removed soon by debug flag removeAfterFSStat' )
+        } catch ( err ) {
+          t.fail( 'failed to create file' )
+        }
+      },
+      function () {
+        try {
+          fs.readFileSync( filepath2 )
+          t.fail( 'file was not removed correctly with debug flag removeAfterFSStat' )
+        } catch ( err ) {
+          t.equal(
+            err.code,
+            'ENOENT',
+            'file was removed between FSStat correctly'
+          )
+        }
+
+        var content = ( 'module.exports = ' + timestamp )
+        fs.writeFileSync( filepath2, content )
+      },
+      function () {
+        w.add( filepath )
+        fs.writeFileSync( filepath, 'module.exports = 22' )
+      },
+    ]
+
+    setTimeout( next, ACTION_INTERVAL )
+
+    function next () {
+      var a = actions.shift()
+      if ( a ) {
+        a()
+        setTimeout( next, ACTION_INTERVAL )
+      } else {
+        finish()
+      }
+    }
+
+    function finish () {
+      t.deepEqual(
+        buffer,
+        expected,
+        'expected output OK'
+      )
+
+      t.equal(
+        w.getLog( filepath2 )[ 'FSStatReadFileSyncErrors' ],
+        1,
+        'FSStatReadFileSyncErrors OK ( 1 )'
+      )
+
+      t.deepEqual(
+        w.getWatched(),
+        [
+          filepath,
+          filepath2
+        ],
+        'expected files (2) still being watched'
+      )
+
+      w.unwatch( filepath )
+
+      t.deepEqual(
+        w.getWatched(),
+        [ filepath2 ],
+        'expected files (1) still being watched'
+      )
+
+      w.unwatch( '**' )
+
+      t.deepEqual(
+        w.getWatched(),
+        [],
+        'expected files (0) still being watched'
+      )
+
+      w.close()
+
+      setTimeout( function () {
+        t.end()
+      }, 100 )
+    }
+  })
+})
+
 test( 'exit process after watcher is closed', function ( t ) {
+  t.timeoutAfter( 5000 )
+
+  prepareTestFiles(function () {
+    var filepath = path.join( __dirname, 'tmp', 'close.js' )
+
+    var expected = [
+      ''
+      , 'ENOENT'
+      , 'module.exports = 777'
+      , 'init: ' + filepath
+      , 'result: 777'
+      , 'watched files: ' + filepath
+      , 'closing watcher instance'
+      , 'exiting: 999'
+      , ''
+    ]
+
+    var buffer = ['\n']
+
+    t.ok(
+      verifyFileCleaning(
+        [
+          filepath
+        ]
+      ),
+      'test pre-cleaned properly'
+    )
+
+    process.env.MITERU_LOGLEVEL = 'silent'
+
+    var spawn = childProcess.spawn('node', [
+      path.join( __dirname, 'test-close.js' )
+    ])
+
+    spawn.stdout.on( 'data', function ( data ) {
+      buffer.push( data.toString( 'utf8' ) )
+    })
+
+    spawn.stderr.on( 'data', function ( data ) {
+      buffer.push( data.toString( 'utf8' ) )
+    })
+
+    spawn.on( 'exit', function ( code ) {
+      finish()
+    })
+
+    function finish () {
+      t.deepEqual(
+        buffer.join( '' ).split( /[\r\n]+/g ).map(function ( line ) {
+          return line.trim()
+        }),
+        expected,
+        'expected output OK'
+      )
+
+      setTimeout( function () {
+        t.end()
+      }, 100 )
+    }
+  })
+})
+
+test( 'process exits when no files being watched', function ( t ) {
   t.timeoutAfter( 5000 )
 
   prepareTestFiles(function () {
@@ -432,11 +793,13 @@ test( 'exit process after watcher is closed', function ( t ) {
       , 'init: ' + filepath
       , 'result: 777'
       , 'watched files: ' + filepath
-      , 'closing watcher instance'
+      , 'watched files:'
+      , 'process should remain active'
       , 'exiting: 999'
+      , ''
     ]
 
-    var buffer = ['']
+    var buffer = ['\n']
 
     t.ok(
       verifyFileCleaning(
@@ -461,13 +824,26 @@ test( 'exit process after watcher is closed', function ( t ) {
       buffer.push( data.toString( 'utf8' ) )
     })
 
+    var _exited = false
+    var _killed = false
+
+    var _t = setTimeout(function () {
+      if ( !_exited ) t.fail( 'spawn failed to exit on its own' )
+      var _killed = true
+      spawn.kill()
+    }, 5000)
+
     spawn.on( 'exit', function ( code ) {
+      t.equal( _killed, false, 'spawn was not killed' )
+      t.equal( _exited, false )
+      _exited = true
       finish()
     })
 
     function finish () {
+      clearTimeout( _t )
       t.deepEqual(
-        buffer.map(function ( line ) {
+        buffer.join( '' ).split( /[\r\n]+/g ).map(function ( line ) {
           return line.trim()
         }),
         expected,
