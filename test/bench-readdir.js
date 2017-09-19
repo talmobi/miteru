54ffc6d8f4bbbfacd888fbe805634cd017ef7404
Christian Talmo <talmo.christian@gmail.com>
2017-09-20 02:16:59 +0300

    moved bench-readdir.js

--- test/bench-readdir.js
+++ test/bench-readdir.js
@@ -1,22 +1,22 @@
-var fs = require('fs')
+var fs = require( 'fs' )
 
 var ITERATIONS = 1000 * 1
 var startTime = Date.now()
 var deltas = []
 var name = ''
 
-function test (callback) {
+function test ( callback ) {
   var now = Date.now()
 
-  fs.readdir( '.' , function (err, files) {
-    if (err) throw err
+  fs.readdir( '.', function ( err, files ) {
+    if ( err ) throw err
 
     var delta = Date.now() - now
     callback( delta )
-  })
+  } )
 }
 
-function testSync (callback) {
+function testSync ( callback ) {
   var now = Date.now()
 
   var files = fs.readdirSync( '.' )
@@ -29,29 +29,29 @@ function run ( t, next ) {
   startTime = Date.now()
   deltas = []
 
-  for (var i = 0; i < ITERATIONS; i++) {
-    t(function ( delta ) {
+  for ( var i = 0; i < ITERATIONS; i++ ) {
+    t( function ( delta ) {
       deltas.push( delta )
 
-      if (deltas.length === ITERATIONS) {
+      if ( deltas.length === ITERATIONS ) {
         finish()
         next && next()
       }
-    })
+    } )
   }
 }
 
 name = '[    ] '
 run( test, function () {
-name = '[sync] '
+  name = '[sync] '
   run( testSync )
-})
+} )
 
 function finish () {
   var sum = 0
-  deltas.forEach(function ( delta ) {
+  deltas.forEach( function ( delta ) {
     sum += delta
-  })
-  console.log( name + 'done, avg delta: ' + ( sum / deltas.length ))
-  console.log( name + 'total time: ' + ( Date.now() - startTime ))
+  } )
+  console.log( name + 'done, avg delta: ' + ( sum / deltas.length ) )
+  console.log( name + 'total time: ' + ( Date.now() - startTime ) )
 }
