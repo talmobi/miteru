bedaf6e3a1af9eeb060f7fcf2246c3b6f5b073a1
Christian Talmo <talmo.christian@gmail.com>
2020-08-07 10:14:12 +0300

    test: require wtfnode for debugging

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ var rimraf = require( 'rimraf' )
 var mkdirp = require( 'mkdirp' )
 var ncp = require( 'ncp' ).ncp
 
+var wtfnode = require( 'wtfnode' )
+
 var fs = require( 'fs' )
 var path = require( 'path' )
 
